0b41a5b8540feb810fc1809bc33589ad3df86a2d
Jakob Wanner <deadzac@gmx.net>
2015-05-29 15:18:04 +0200

    Extract first paragraph from Wikipedia

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,15 +1,19 @@
+var wikiResponse = {};
 var wikipedia  = {
 
-    initialize: function() {
+    initialize: function(data) {
+        this.addPlaceToMap = data.addPlaceToMap;
 
     },
     search: function(term) {
 
        // var url = 'http://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
 
-        var url = 'http://en.wikipedia.org/w/api.php?action=query&titles=' +
-            encodeURIComponent(term) +
-            '&format=json';//&prop=revisions&rvprop=content
+        //var url = 'http://en.wikipedia.org/w/api.php?action=query&titles=' +
+        //var url = 'http://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&page=' +
+        var url = ' http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=' +
+            encodeURIComponent(term);
+            //+ '&format=json';//&prop=revisions&rvprop=content
 
         //'/w/api.php?action=query&list=allpages&prop=info&format=json&apfrom=Hamburg';
         $.ajax( {
@@ -17,11 +21,19 @@ var wikipedia  = {
             //data: queryData,
             dataType: 'jsonp',
             jsonp: 'callback',
+            crossDomain: true,
             type: 'POST',
             headers: { 'Api-User-Agent': 'udacity-course/1.0' }
         }).done(function(data) {
-            console.log('wikipedia');
-            console.log(data);
+            //console.log('wikipedia');
+            //console.log(data);
+            wikiResponse = data;
+            var pages = data.query.pages;
+            for(var index in pages) {
+                if(pages.hasOwnProperty(index)) {
+                    console.log(pages[index].extract);
+                }
+            }
         }).fail(function() {
             console.log('wikipedia failed');
         });
